c06e578acf9257382169990f7aa02103a2ced0a2
divrawat <divrawat2001@gmail>
2024-07-03 00:26:14 +0530

    first commit

---
+++ config.js
@@ -0,0 +1,331 @@
+
+// export const DOMAIN = 'http://localhost:3000';
+export const DOMAIN = 'https://www.kallithechampion.in.net';
+
+export const MANGA_DESCRIPTION = `<a href="${DOMAIN}"><u>Kalli The Champion</u></a> is a dark fantasy web novel originally written by Baeksu Noble. It follows the story of Park Kang-tae, a man who harbored a deep regret – giving up on his dream of becoming a boxer. After a violent end as a gang member, he finds himself reincarnated as Kalli, a slave in a world obsessed with a brutal sport called "pugilist games."`;
+
+export const MANGA_NAME = 'Kalli The Champion';
+export const MANGA_AUTHOR = 'Baeksu Noble';
+export const MANGA_RELEASE = '2015';
+export const MANGA_STATUS = 'Ongoing';
+export const MANGA_ARTIST = 'Baeksu Noble';
+export const MANGA_STUDIO = 'None';
+export const MANGA_GENRE = 'Action, Fantasy, Drama, Sports';
+
+export const CHAPTER_PREFIX = 'kalli-the-champion-chapter';
+export const IMAGE_PREFIX = 'images/kalli-the-champion';
+export const NEXT_PREVIOUS_PREFIX = 'manga/kalli-the-champion-chapter';
+export const URL_PREFIX = 'manga/kalli-the-champion-chapter';
+
+export const AUTHOR_PAGE = `${DOMAIN}/kalli-the-champion-team`;
+export const LOGO_URL = `${DOMAIN}/logo.webp`;
+export const COVER_IMG = `${DOMAIN}/cover.webp`;
+export const BEHIND_COVER_IMG = "https://www.ascomaxx.com/uploads/large/b1affb913a4c091b96b622a2c3cb0707.jpg";
+
+
+// Navbar
+export const logo = <img src={`${LOGO_URL}`} alt="Logo" className="h-[75px] rounded-full w-[75px] mr-5 md:ml-0 ml-4 md:pb-2 md:my-0 my-1" />
+
+export const APP_NAME = 'Kalli The Champion';
+export const NavbarName = "Kalli The Champion";
+
+export const APP_DESCRIPTION = `Read ${MANGA_NAME} manga online at ${DOMAIN}. It follows the story of Park Kang-tae, a man who harbored a deep regret – giving up on his dream of becoming a boxer. After a violent end as a gang member, he finds himself reincarnated as Kalli, a slave in a world obsessed with a brutal sport called "pugilist games.`;
+
+export const DISQUS_SHORTNAME = "my-cms-7";
+
+
+export const MANGA_SUMMARY = [
+    {
+        id: 1,
+        content: "Kalli the Champion is a dark fantasy web novel originally written by Baeksu Noble. It follows the story of Park Kang-tae, a man who harbored a deep regret – giving up on his dream of becoming a boxer. After a violent end as a gang member, he finds himself reincarnated as Kalli, a slave in a world obsessed with a brutal sport called pugilist games."
+    },
+    {
+        id: 2,
+        content: "This world offers a twisted take on boxing, where gladiatorial combat is a major form of entertainment. Here, Kalli gets a second chance to pursue his boxing dream. However, his path is fraught with challenges. As a slave, he faces discrimination and harsh training regimes. The story delves into the dark side of this gladiatorial world, with themes of violence, slavery, and potentially sexual assault (reader discretion is advised)."
+    },
+    {
+        id: 3,
+        content: "Kalli the Champion also has a webtoon adaptation. This version, titled simply Kalli the Champion, is written by U130 with art by El Macho. It presumably follows the same core story as the web novel, but with the added power of visual storytelling."
+    },
+];
+
+
+
+
+export const navLinks = [
+    { text: 'Home', href: `${DOMAIN}` },
+    { text: 'DMCA', href: `${DOMAIN}/dmca` },
+    { text: 'Terms & Conditions', href: `${DOMAIN}/terms-and-conditions` },
+];
+
+export const FooterLinks = [
+    { text: 'About', href: `${DOMAIN}/about` },
+    { text: 'Contact', href: `${DOMAIN}/contact` },
+    { text: 'Disclaimer', href: `${DOMAIN}/disclaimer` },
+    { text: 'Privacy Policy', href: `${DOMAIN}/privacy-policy` },
+    { text: 'Terms & Conditions', href: `${DOMAIN}/terms-and-conditions` }
+];
+
+
+
+export const RelatedMangaLinks = [
+    /*
+    {
+        imageUrl: `${DOMAIN}/related/frieran.webp`,
+        title: 'Frieran Manga',
+        link: 'https://www.frierenmanga.in.net',
+    },
+    */
+
+];
+
+
+
+
+
+export const chaptersData = [
+    {
+        "chapterNumber": "1",
+        "numImages": 22
+    },
+    {
+        "chapterNumber": "2",
+        "numImages": 15
+    },
+    {
+        "chapterNumber": "3",
+        "numImages": 14
+    },
+    {
+        "chapterNumber": "4",
+        "numImages": 17
+    },
+    {
+        "chapterNumber": "5",
+        "numImages": 12
+    },
+    {
+        "chapterNumber": "6",
+        "numImages": 12
+    },
+    {
+        "chapterNumber": "7",
+        "numImages": 13
+    },
+    {
+        "chapterNumber": "8",
+        "numImages": 14
+    },
+    {
+        "chapterNumber": "9",
+        "numImages": 17
+    },
+    {
+        "chapterNumber": "10",
+        "numImages": 14
+    },
+    {
+        "chapterNumber": "11",
+        "numImages": 16
+    },
+    {
+        "chapterNumber": "12",
+        "numImages": 16
+    },
+    {
+        "chapterNumber": "13",
+        "numImages": 15
+    },
+    {
+        "chapterNumber": "14",
+        "numImages": 14
+    },
+    {
+        "chapterNumber": "15",
+        "numImages": 13
+    },
+    {
+        "chapterNumber": "16",
+        "numImages": 15
+    },
+    {
+        "chapterNumber": "17",
+        "numImages": 13
+    },
+    {
+        "chapterNumber": "18",
+        "numImages": 13
+    },
+    {
+        "chapterNumber": "19",
+        "numImages": 12
+    },
+    {
+        "chapterNumber": "20",
+        "numImages": 12
+    },
+    {
+        "chapterNumber": "21",
+        "numImages": 75
+    },
+    {
+        "chapterNumber": "22",
+        "numImages": 67
+    },
+    {
+        "chapterNumber": "23",
+        "numImages": 66
+    },
+    {
+        "chapterNumber": "24",
+        "numImages": 10
+    },
+    {
+        "chapterNumber": "25",
+        "numImages": 9
+    },
+    {
+        "chapterNumber": "26",
+        "numImages": 10
+    },
+    {
+        "chapterNumber": "27",
+        "numImages": 9
+    },
+    {
+        "chapterNumber": "28",
+        "numImages": 9
+    },
+    {
+        "chapterNumber": "29",
+        "numImages": 13
+    },
+    {
+        "chapterNumber": "30",
+        "numImages": 13
+    },
+    {
+        "chapterNumber": "31",
+        "numImages": 14
+    },
+    {
+        "chapterNumber": "32",
+        "numImages": 15
+    },
+    {
+        "chapterNumber": "33",
+        "numImages": 16
+    },
+    {
+        "chapterNumber": "34",
+        "numImages": 16
+    },
+    {
+        "chapterNumber": "35",
+        "numImages": 16
+    },
+    {
+        "chapterNumber": "36",
+        "numImages": 15
+    },
+    {
+        "chapterNumber": "37",
+        "numImages": 13
+    },
+    {
+        "chapterNumber": "38",
+        "numImages": 15
+    },
+    {
+        "chapterNumber": "39",
+        "numImages": 13
+    },
+    {
+        "chapterNumber": "40",
+        "numImages": 24
+    },
+    {
+        "chapterNumber": "41",
+        "numImages": 25
+    },
+    {
+        "chapterNumber": "42",
+        "numImages": 12
+    },
+    {
+        "chapterNumber": "43",
+        "numImages": 15
+    },
+    {
+        "chapterNumber": "44",
+        "numImages": 14
+    },
+    {
+        "chapterNumber": "45",
+        "numImages": 15
+    },
+    {
+        "chapterNumber": "46",
+        "numImages": 22
+    },
+    {
+        "chapterNumber": "47",
+        "numImages": 17
+    },
+    {
+        "chapterNumber": "48",
+        "numImages": 28
+    },
+    {
+        "chapterNumber": "49",
+        "numImages": 17
+    },
+    {
+        "chapterNumber": "50",
+        "numImages": 13
+    },
+    {
+        "chapterNumber": "51",
+        "numImages": 17
+    },
+    {
+        "chapterNumber": "52",
+        "numImages": 25
+    },
+    {
+        "chapterNumber": "53",
+        "numImages": 18
+    },
+    {
+        "chapterNumber": "54",
+        "numImages": 25
+    },
+    {
+        "chapterNumber": "55",
+        "numImages": 18
+    },
+    {
+        "chapterNumber": "56",
+        "numImages": 22
+    },
+    {
+        "chapterNumber": "57",
+        "numImages": 12
+    },
+    {
+        "chapterNumber": "58",
+        "numImages": 9
+    },
+    {
+        "chapterNumber": "59",
+        "numImages": 10
+    },
+    {
+        "chapterNumber": "60",
+        "numImages": 11
+    },
+    {
+        "chapterNumber": "61",
+        "numImages": 20
+    }
+]
